52eb339bfc2b28fe66fe81b21e7e4694c0364dcc
Minseo <msp05070478@gmail.com>
2022-01-15 16:42:23 +0900

    역할 분담

---
+++ app/src/main/java/com/example/hightone/jeongwoo/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hightone.jeongwoo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hightone.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
---
+++ app/src/main/java/com/example/hightone/minseo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hightone.minseo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hightone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
