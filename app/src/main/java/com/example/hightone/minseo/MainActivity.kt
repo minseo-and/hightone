68fc363b43d72d5ccab831033af736ca062d0503
minseo-and <msp05070478@gmail.com>
2022-01-15 17:45:12 +0900

    역할

--- app/src/main/java/com/example/hightone/minseo/MainActivity.kt
+++ app/src/main/java/com/example/hightone/minseo/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hightone
+package com.example.hightone.minseo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hightone.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
